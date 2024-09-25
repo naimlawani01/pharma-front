a5513a7a9464ebab8c12db0db611f2d7c267e24d
Najath Lawani <najathlawani73@gmail.com>
2024-09-25 15:39:59 +0000

    login page img

--- src/Routes/publicsRoutes.js
+++ src/Routes/publicsRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes} from 'react-router-dom';
-import LoginPage from '../pages/LoginPage'
+import LoginPage from '../pages/loginPage'
 
 
 const AppRoutes = ()=> {
