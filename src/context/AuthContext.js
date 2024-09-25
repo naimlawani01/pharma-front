28b0f2dbafaea488acadaef5278352bc39a7a5af
najath73 <146420142+najath73@users.noreply.github.com>
2024-09-25 11:55:13 +0200

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -14,6 +14,7 @@ export const AuthProvider = ({ children }) => {
             setError(null);
         } catch (err) {
             setError(err.message);
+            throw err;
         }
     };
 
