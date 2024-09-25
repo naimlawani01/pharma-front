360872233939fbf25840edeeefa2d7b4420be9e4
najath73 <146420142+najath73@users.noreply.github.com>
2024-09-25 11:56:31 +0200

    Update publicsRoutes.js

--- src/Routes/publicsRoutes.js
+++ src/Routes/publicsRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes} from 'react-router-dom';
-import LoginPage from '../pages/loginPage'
+import LoginPage from '../pages/LoginPage'
 
 
 const AppRoutes = ()=> {
