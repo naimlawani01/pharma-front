dc9d2fba2ed91e0fe6d0ac2de5f76309dd183286
Najath Lawani <najathlawani73@gmail.com>
2024-09-30 11:23:33 +0000

    change application name

--- src/Routes/publicsRoutes.js
+++ src/Routes/publicsRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes} from 'react-router-dom';
-import LoginPage from '../pages/LoginPage'
+import LoginPage from '../pages/loginPage'
 import HomePage from '../pages/homePage';
 
 
